1987df203d2463d818f83d98c12a760d4c56611a
Aliseiko <aliseiko.dev@gmail.com>
2023-06-06 13:05:33 +0300

    feat: File system create file done

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,16 @@
+import * as fs from 'node:fs';
+import path from 'node:path';
+
 const create = async () => {
-    // Write your code here 
+    const filePath = path.join(path.dirname('create.js'), '/files/fresh.txt');
+    if (fs.existsSync(filePath)) {
+        throw new Error('FS operation failed');
+    } else {
+        fs.writeFile(filePath, 'I am fresh and young', (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+        })
+    }
 };
 
 await create();
